a2f799c2a7dc199e921f7c65028a91bb15c8583c
Shinku <patrickhaselof@icloud.com>
2026-02-28 18:07:45 -0300

    Commit 2 - Exploring
    - Exploring my possibilities
    - Building and registering a strategy
    - Testing in-built functions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # My-Galgos-Task
-A University related project
+
+Neste projeto estaremos explorando uma possivel solucao para o problema do caixeiro viajante, um problema NP-Dificil, ou seja, nao possui uma solucao eficiente conhecida.
+
+Minha primeira ideia de como abordar o problema seria tentar utilizar um algoritmo eficiente que eu ja estou familiarizado, o A*. A ideia era selecionar um ponto inicial e um final para executar o algoritmo e depois explorar as possiveis combinacoes que proporcionam um caminho "aceitavel", depois rodar o algoritmo novamente utilizando o caminho reverso e as impondo restricoes necessarias. Mas depois percebi que em modelos muito grandes seria inviavel.
+
+Entao comecei a procurar por outras solucoes, como o algoritmo guloso do Vizinho mais proximo, dado que eu li que uma abordagem Heuristica ou Meta-heuristica no meu caso seria a mais eficaz. Mas eu gostaria de usar uma solucao mais sofisticada, o que me deu a ideia de combinar algoritmos como Held-Karp e Colonia de Formigas.
+
+
--- Task.py
+++ Task.py
@@ -4,15 +4,47 @@ import tsplib95 # pyright: ignore[reportMissingImports]
 task = tsplib95.load('berlin52.tsp')
 
 #View the files contents
+print("--------------------------------------------------------------------------------------------------------------------")
 print("[NAME]: " + str(task.name))
 print("[DIMENSION]: " + str(task.dimension))
 print("[WEIGHT_TYPE]: " + str(task.edge_weight_type))
 print("[WEIGHT_FORMAT]: " + str(task.edge_weight_format))
 print("[NODE_TYPE]: " + str(task.node_coord_type))
 print("[NODE_DIPLAY]: " + str(task.display_data_type))
-print("[COORDS]: " + str(task.node_coords))
-print("----------------------------------------------------------------------------------------------------------------------------\n")
+print("--------------------------------------------------------------------------------------------------------------------\n")
+
+'''
+coords = task.node_coords
 
 #Find node weight test
 weight = task.get_weight(1, 2)
-print(f"Weight between node 1 and 2: {weight}")
+print("Weight(1, 2) - " + str(weight))
+
+coords = task.node_coords
+
+# Access specific coordinate test
+node_1_x, node_1_y = coords[1]
+print("X - " + str(node_1_x))
+print("Y - " + str(node_1_y))
+'''
+
+#Functions
+def closest_path(node, size, map):
+    current = -1
+    shortest = -1
+    i = 1
+    while(i <= 52):
+        if(i != node):
+            current = map.get_weight(node, i)
+            if(shortest == -1):
+                shortest = current
+            elif(current < shortest):
+                shortest = current
+        i+=1
+    
+    return shortest
+
+
+#Main
+coords = task.node_coords
+print(closest_path(1, 52, task))
